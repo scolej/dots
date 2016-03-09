f945618bea370f329e3754ab65a1588dd22dada6
unknown <t0165486@34617D.au.thalesgroup.local>
2016-03-10 08:54:27 +1100

    changes

--- emacs.el
+++ emacs.el
@@ -99,4 +99,23 @@
 ;;        C-q C-[ escape
 ;; M-x find-named-dired
 
+(defun duplicate-line ()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank))
+(global-set-key (kbd "C-d") 'duplicate-line)
+
+(setq ido-auto-merge-work-directories-length 5)
+
+;; (defun copy-line ()
+;;   (interactive)
+;;   (kill-ring-save (line-beginning-position)
+;;                   (line-beginning-position 1)))
+
+(global-set-key (kbd "C-c C-c") (kbd "C-a <C-SPC> C-e M-w"))
+
 (server-start)
