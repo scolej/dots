d41c9281e471ed822cdc51f82b98c1f1a5607e9f
sjc <s.cole.j@gmail.com>
2016-04-30 10:12:12 +1000

    tweak

--- emacs.el
+++ emacs.el
@@ -1,12 +1,11 @@
 (require 'package)
 (package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (require 'drag-stuff)
 (require 'duplicate-thing)
 (require 'highlight-symbol)
-
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; Disable annoying things
 (setq inhibit-startup-message t)
