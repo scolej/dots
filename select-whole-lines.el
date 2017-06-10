b10f0bca56678cb2cf8626b39022253cf3780c9a
sjc <s.cole.j@gmail.com>
2017-06-10 13:03:18 +1000

    A minor mode for selecting whole lines with shift-up/down motions.

---
+++ select-whole-lines.el
@@ -0,0 +1,60 @@
+;;; package -- Handy line selection.
+
+;;; Commentary:
+
+;;; Code:
+
+(defvar-local selecting-whole-lines nil)
+(defvar-local selecting-whole-lines-restore-column nil)
+
+(defun select-whole-lines (direction)
+  "Move the cursor and set mark to facilitate easy selection of whole lines.
+DIRECTION is either 'up or 'down."
+  (interactive)
+  (cond ((and (region-active-p)
+              (null selecting-whole-lines))
+         (if (eq direction 'up)
+             (previous-line)
+           (next-line)))
+        ((and (region-active-p)
+              selecting-whole-lines)
+         (if (eq direction 'up)
+             (forward-line -1)
+           (forward-line)))
+        ((null (region-active-p))
+         (transient-mark-mode t)
+         (setq selecting-whole-lines t)
+         (setq selecting-whole-lines-restore-column (current-column))
+         (if (eq direction 'up)
+             (progn (beginning-of-line 2)
+                    (set-mark-command nil)
+                    (forward-line -1))
+           (beginning-of-line)
+           (set-mark-command nil)
+           (forward-line 1)))))
+
+(defun select-whole-lines-up ()
+  "Select the current line in preparation for extending the selection upwards."
+  (interactive)
+  (select-whole-lines 'up))
+
+(defun select-whole-lines-down ()
+  "Select the current line in preparation for extending the selection downwards."
+  (interactive)
+  (select-whole-lines 'down))
+
+(define-minor-mode select-whole-lines-mode
+  "Easily select whole lines."
+  :lighter " wl"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "<S-down>") 'select-whole-lines-down)
+            (define-key map (kbd "<S-up>") 'select-whole-lines-up)
+            map)
+  (add-hook 'deactivate-mark-hook
+            (lambda ()
+              (when (and selecting-whole-lines
+                         selecting-whole-lines-restore-column)
+                (move-to-column selecting-whole-lines-restore-column))
+              (setq selecting-whole-lines nil))))
+
+;;; select-whole-lines.el ends here
