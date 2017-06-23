1814eb84e416e22b6aa9830cdb7a27225560da4b
sjc <s.cole.j@gmail.com>
2017-06-23 23:18:45 +1000

    Make copy and paste smarter.

--- select-whole-lines.el
+++ select-whole-lines.el
@@ -4,6 +4,7 @@
 
 ;;; Code:
 
+(defvar-local select-whole-lines-yank nil)
 (defvar-local selecting-whole-lines nil)
 (defvar-local selecting-whole-lines-restore-column nil)
 
@@ -62,5 +63,28 @@ DIRECTION is either 'up or 'down."
                          selecting-whole-lines-restore-column)
                 (move-to-column selecting-whole-lines-restore-column))
               (setq selecting-whole-lines nil))))
+
+(defun select-whole-lines-yank-setter ()
+  "Set a flag to indicate whether a yank was performed while selecting whole lines."
+  (if selecting-whole-lines
+      (setq select-whole-lines-yank t)
+    (setq select-whole-lines-yank nil)))
+
+(defadvice kill-ring-save (before select-whole-lines-saver activate)
+  "Conditionally set the select-whole-lines-yank flag."
+  (select-whole-lines-yank-setter))
+
+(defadvice kill-region (before select-whole-lines-killer activate)
+  "Conditionally set the select-whole-lines-yank flag."
+  (select-whole-lines-yank-setter))
+
+(defadvice yank (before select-whole-lines-yanker activate)
+  "Conditionally jump to the start of the line.
+If the previous kill/save was done whilst selecting whole lines,
+then we should jump to the start of the line before the yank.
+Otherwise no extra action is performed."
+  (when select-whole-lines-yank (beginning-of-line)))
+
+(provide 'select-whole-lines)
 
 ;;; select-whole-lines.el ends here
