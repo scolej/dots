0dc0b829ccad05ac1ef655365466afa136552e63
sjc <s.cole.j@gmail.com>
2019-01-01 21:07:17 +1100

    New year, new config!

--- emacs/emacs2.el
+++ emacs/emacs2.el
@@ -1,9 +1,8 @@
-;; (require 'package)
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; (package-initialize)
+(setq inhibit-splash-screen t
+      initial-scratch-message nil)
 
-(setq inhibit-splash-screen t)
+(setq tab-width 4
+      indent-tabs-mode nil)
 
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -11,14 +10,53 @@
 (blink-cursor-mode -1)
 (show-paren-mode 1)
 
-(setq auto-save-timeout 20)
+;; Still blasts Eldoc message :(
+;; (setq auto-save-visited-interval 1)
+;; (auto-save-visited-mode 1)
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+(global-set-key (kbd "C-x C-s") 'save-all)
+
+(setq-default sentence-end-double-space nil)
+(setq-default mode-line-format '("%* %b"))
 
 (global-set-key (kbd "C-z") 'undo)
 
+(require 'dired)
+(add-hook 'dired-mode-hook #'dired-hide-details-mode)
+(add-hook 'dired-mode-hook #'hl-line-mode)
+(define-key dired-mode-map (kbd "<backspace>") #'dired-up-directory)
+(setq dired-recursive-deletes 'always)
+
 (defun kill-this-buffer ()
-  "Kill the current buffer."
+  "Kill the current buffer. Ask no questions."
   (interactive)
   (kill-buffer))
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 (setf ansi-color-for-comint-mode 'filter)
+
+(define-derived-mode wrapping-text-mode fundamental-mode
+  (toggle-truncate-lines -1)
+  (visual-line-mode t))
+(add-to-list 'auto-mode-alist '("\\.tx\\'" . wrapping-text-mode))
+
+(defun call-maybe-with-region (fun)
+  "Build a function which calls FUN interactively, inserting the
+region into minibuffer if it is active."
+  (lexical-let ((fun fun))
+    (lambda ()
+      (interactive)
+      (let ((init (if (region-active-p)
+		              (buffer-substring-no-properties (point) (mark))
+		            nil)))
+	    (minibuffer-with-setup-hook (lambda () (when init (insert init)))
+	      (call-interactively fun))))))
+
+(defun google (term)
+  (interactive "M")
+  (browse-url
+   (concat "https://google.com/search?query="
+           (url-encode-url term))))
+(global-set-key (kbd "C-c g") (call-maybe-with-region 'google))
