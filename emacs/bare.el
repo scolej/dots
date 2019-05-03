9dc63810b2bdfb7a9ca6908a4e0775573e8ec475
sjc <s.cole.j@gmail.com>
2019-05-03 16:43:53 +1000

    : Some.

--- emacs/bare.el
+++ emacs/bare.el
@@ -1,15 +1,16 @@
+
 (setq custom-file "~/.emacs.d/custom.el")
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+(delete-selection-mode 1)
 (global-auto-revert-mode 1)
 (recentf-mode 1)
 (show-paren-mode 1)
 
-(setq indent-tabs-mode nil
-      c-basic-offset 4
+(setq c-basic-offset 4
 
       save-interprogram-paste-before-kill t
 
@@ -30,12 +31,21 @@
       dired-clean-confirm-killing-deleted-buffers nil
       dired-use-ls-dired nil
 
+      require-final-newline nil
+      mode-require-final-newline nil
+
+      truncate-lines t
+      ring-bell-function nil
+      show-trailing-whitespace t
       sentence-end-double-space nil
       set-mark-command-repeat-pop t
       ring-bell-function 'ignore
       linum-format "%4d"
       mouse-autoselect-window -0.1
-      revert-without-query '(".*"))
+      revert-without-query '(".*")
+      mouse-wheel-progressive-speed nil)
+
+(setq-default indent-tabs-mode nil)
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -49,3 +59,15 @@
 
 (global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "M-g") 'goto-line)
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(defun maybe-copy-whole-line ()
+  (interactive)
+  (if mark-active
+      (copy-region-as-kill nil nil t)
+    (beginning-of-line 2)
+    (if (eq last-command 'maybe-copy-whole-line)
+        (kill-append (buffer-substring-no-properties (point) (line-beginning-position 0))
+                     nil)
+    (kill-ring-save (point) (line-beginning-position 0)))))
+(global-set-key (kbd "M-w") 'maybe-copy-whole-line)
