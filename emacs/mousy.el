1fc3b14184e23a0760029dc914145a4ad7955ec8
sjc <s.cole.j@gmail.com>
2019-05-09 21:22:00 +1000

    Crazy macro.

--- emacs/mousy.el
+++ emacs/mousy.el
@@ -1,27 +1,33 @@
-(defun mousy-line-points ()
-  (let* ((height (window-body-height))
-	 (top (save-excursion
-		(set-window-point nil (window-start))
-		(line-number-at-pos)))
-	 (bottom (+ top height -1))
-	 (middle (+ top (/ height 2))))
-    (list top middle bottom)))
+(defmacro with-mousy-points (expr)
+  `(let* ((height (window-body-height))
+	  (top-line (line-number-at-pos (window-start)))
+	  (bottom-line (+ top-line height -1))
+	  (middle-line (+ top-line (/ height 2)))
+          (current-line (line-number-at-pos (point))))
+     ,expr))
+
+(defmacro debug-mousy-points (expr)
+  `(progn
+     (with-mousy-points
+      (progn
+        (message "Before: %i %i %i %i" top-line middle-line bottom-line current-line)
+        ,expr))
+     (with-mousy-points
+      (message "After: %i %i %i %i" top-line middle-line bottom-line current-line))))
 
 (defun mousy-scroll-up ()
   (interactive)
-  (let ((line (line-number-at-pos)))
-    (pcase-let ((`(,top ,middle ,bottom) (mousy-line-points)))
-      (cond ((= line top) (scroll-up))
-	    ((<= line middle) (recenter 0))
-	    ((recenter))))))
+  (debug-mousy-points
+   (cond ((= current-line top-line) (scroll-up))
+	 ((<= current-line middle-line) (recenter 0))
+	 ((recenter)))))
 
 (defun mousy-scroll-down ()
   (interactive)
-  (let ((line (line-number-at-pos)))
-    (pcase-let ((`(,top ,middle ,bottom) (mousy-line-points)))
-      (cond ((= line bottom) (scroll-down))
-	    ((>= line middle) (recenter -1))
-	    ((recenter))))))
+  (debug-mousy-points
+   (cond ((= current-line bottom-line) (scroll-down))
+	 ((>= current-line middle-line) (recenter -1))
+	 ((recenter)))))
 
 (defconst mousy-mode-map (make-sparse-keymap))
 ;; (define-key mousy-mode-map (kbd "<wheel-up>") 'mousy-scroll-up)
